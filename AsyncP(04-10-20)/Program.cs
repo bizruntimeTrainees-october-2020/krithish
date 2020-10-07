b81beac60519a8d60c977d145b3a6a57590ac78f
Kitty <krithish97@gmail.com>
2020-10-07 19:23:07 +0530

    AsynchronousProgramming

---
+++ AsyncP(04-10-20)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncP
+{
+    class AsyncDemo
+    {
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("METHOD 1 IS EXECUTING");
+                }
+            });
+        }
+
+        public static void Method2()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("METHOD 2 IS EXECUTING");
+            }
+        }
+
+        public static  async Task Method3()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("METHOD 3 IS EXECUTING");
+                }
+            });
+        }
+        static void Main(string[] args)
+        {
+            Method1();
+
+            
+            Method2();
+            Method3();
+
+
+        }
+    }
+}
---
+++ SyncAsync(05-10-10)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ThreadingExample
+
+{
+     
+public class MyThread
+    {
+        public void Thread1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(400);
+
+
+            }
+            Task t = Task.Run(() =>
+          
+
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                   
+                    Console.Write("Hi ");
+                    Thread.Sleep(500);
+                }
+                
+            });
+
+            t.Wait();
+        }
+    }
+
+    
+    public class ThreadEx
+    {
+       
+            public static void Main()
+        {
+            MyThread mt = new MyThread();
+            Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+            Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+            Thread t3 = new Thread(new ThreadStart(mt.Thread1));
+            t1.Start();
+            t1.Join();
+            t2.Start();
+            
+            t3.Start();
+
+
+           
+        }
+    }
+
+}
+
