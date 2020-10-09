1195008bc75b9b71c11deaa9cadf8b9bfd6c160e
Kitty <krithish97@gmail.com>
2020-10-09 19:06:50 +0530

    Serialization

---
+++ JsonNet(9-10-20)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+c
+
+namespace SerializeDeSerialize
+{
+    public class MovieDetails
+    {
+
+
+        class Movie
+        {
+            public int Id { get; set; }
+            public string title { get; set; }
+            public string ShowTimings { get; set; }
+        }
+        static void Main(string[] args)
+            {
+            Console.WriteLine("\n-->> SERIALIZATION <<--");
+            Movie m = new Movie { Id = 1, title = "Billa", ShowTimings="Night-Show" };
+            String result = JsonConvert.SerializeObject(m);
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("\n-->>  DE-SERIALIZATION <<--");
+            Movie mu = JsonConvert.DeserializeObject<Movie>(result);
+            Console.WriteLine("Id is  : " + mu.Id);
+            Console.WriteLine("Title is  : " + mu.title);
+            Console.WriteLine("Show Timings is   : " + mu.ShowTimings);
+        }
+        }
+
+    }
---
+++ SerializationDeserialzation(BINARY)(09-10-20)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace SerializationDemo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.rno = 1;
+            s1.name = "John Smith";
+            s1.marks = 87.5;
+            IFormatter f = new BinaryFormatter();
+            Stream str = new FileStream(@"C:\Users\krithish\source\repos\SerializationDeserialzation\Test.txt", FileMode.Create, FileAccess.Write);
+            f.Serialize(str, s1);
+
+            str.Close();
+            str = new FileStream(@"C:\Users\krithish\source\repos\SerializationDeserialzation\Test.txt", FileMode.Open, FileAccess.Read);
+            Student s2 = (Student)f.Deserialize(str);
+
+            Console.WriteLine("Roll Number: {0}", s2.rno);
+            Console.WriteLine("Name: {0}", s2.name);
+            Console.WriteLine("Marks: {0}", s2.marks);
+        }
+    }
+    [Serializable]
+    class Student
+    {
+        public int rno;
+        public String name;
+        public double marks;
+    }
+}
