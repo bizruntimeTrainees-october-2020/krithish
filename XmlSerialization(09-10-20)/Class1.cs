0bc6aa83b146fb6e46ae5c9e8b9a72157d36b377
Kitty <krithish97@gmail.com>
2020-10-10 11:22:55 +0530

    name of the topic Serialization

---
+++ XmlSerialization(09-10-20)/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    public class Class1
+    {
+        public int Age { get; set; }
+        public bool Male { get; set; }
+        [XmlAttribute]
+        public string Name { get; set; }
+        public void Save(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Create))
+            {
+                var XML = new XmlSerializer(typeof(Class1));
+                XML.Serialize(stream, this);
+            }
+
+        }
+
+        public Class1 LoadFromFile(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Open))
+            {
+                var XML = new XmlSerializer(typeof(Class1));
+                return (Class1)XML.Deserialize(stream);
+            }
+        }
+    }
+
+}
---
+++ XmlSerialization(09-10-20)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace XmlSerialization
+{
+
+    public class Program
+    {
+        #region properties
+
+        public List<string> UIComponents { get; set; }
+        public List<string> Settings { get; set; }
+
+        #endregion
+
+       
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Age = 24;
+            class1.Name = "Krithish";
+            class1.Male = true;
+            class1.Save("Example.xml");
+
+
+        }
+
+    }  }
