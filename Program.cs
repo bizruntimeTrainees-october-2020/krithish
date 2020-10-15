6760acec77080666eaac446762563095f59f7205
krithish b <krithish97@gmail.com>
2020-10-15 19:18:09 +0530

    loggers

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+namespace Loggerss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            .WriteLine("Hello World!");
+        }
+    }
+}
