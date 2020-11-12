983e481fedcbfa791ce24d2801d6b6790697af12
krithish b <krithish97@gmail.com>
2020-11-12 18:49:42 +0530

    Multiple Clients

---
+++ ClientSide/ClientSide/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+namespace ClientSide
+{
+    class MyClient
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAddress = "127.0.0.1";
+            Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+            ClientSocket.Connect(ep);
+            Console.WriteLine("Client Is Ready Type your Message....");
+            
+            
+            while (true)
+            {
+                string messageFromClient = null;
+                Console.WriteLine("Enter the Message");
+                messageFromClient = Console.ReadLine();
+                ClientSocket.Send(System.Text.Encoding.ASCII.GetBytes(messageFromClient), 0,
+                    messageFromClient.Length, SocketFlags.None);
+                
+                byte[] MsgFromServer = new byte[1024];
+                int size = ClientSocket.Receive(MsgFromServer);
+                Console.WriteLine("Server" + System.Text.Encoding.ASCII.GetString(MsgFromServer, 0, size));
+
+            }
+
+        }
+    }
+   
+} 
---
+++ ServerSide/ServerSide/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ServerSide
+{
+    class MyServer
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            string IpAddress = "127.0.0.1";
+            Socket ServerListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+            ServerListener.Bind(ep);
+            ServerListener.Listen(100);
+            Console.WriteLine("Server is Listening and Ready....");
+            Socket ClientSocket = default(Socket);
+            int counter = 0; // TO SHOW NO OF CLIENTS 
+            MyServer ms = new MyServer();
+            while (true)
+            {
+                counter++;
+                ClientSocket = ServerListener.Accept();
+                Console.WriteLine(counter + "Client is Connected");
+                Thread UserThread = new Thread(new ThreadStart(() => ms.User(ClientSocket)));
+                UserThread.Start();
+            }
+
+         }
+
+        public void User(Socket client)
+        {
+            while (true)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                client.Send(msg, 0, size, SocketFlags.None);
+            }
+        }
+    }
+}
